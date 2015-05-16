66e78719ab0a17afc20f152d1960fe473e6beb46
Andrea Knabe <andrea.knabe@posteo.de>
2015-05-16 16:37:37 +0200

    license files modified

---
+++ pomodoro_akut.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from subprocess import call
+import time
+import sys
+import webbrowser
+from twitter import *
+
+duration = 20 #Dauer für ein Pomodoro/duration of one pomodoro
+short_brk = 15 #Länge einer kurzen Pause/short break duration
+long_brk = 30 #Länge einer langen Pause/long break duration
+one_pomodori = 5 #Anzahl der Pomodori/number of pomodori
+duration_count = duration
+
+def pomodoro(one_pomodori,short_brk,long_brk,duration,duration_count):
+    for i in range(1,one_pomodori):
+        duration = duration_count
+        while duration > 0:
+            print "%d min working..." % int(duration_count - duration)
+            time.sleep(60)
+            duration -= 1
+
+        #start short break
+        print "Take short %d min break" % short_brk
+        webbrowser.open('http://vimeopro.com/ifamt/funktionelle-uebungen/video/83999082')
+    	t = Twitter(
+    	    auth=OAuth('???', '???', '???', '???'))
+
+    	t.direct_messages.new(
+        user="@???",
+        text = ("IFAM. SOFORT. UNBEDINGT."))
+
+        time.sleep(60*int(short_brk))
+        
+        #stop short break
+        
+        print "Back to work..."
+
+    	t = Twitter(
+    	    auth=OAuth('???', '???', '???', '???'))
+
+    	t.direct_messages.new(
+        user="@???",
+        text = ("WEITERMACHEN. Besser ist."))
+
+    # start long break
+    
+    print "Well done.Take long %d min break" % long_brk
+
+    t = Twitter(
+    auth=OAuth('???', '???', '???', '???'))
+
+    t.direct_messages.new(
+    user="@???",
+    text = ("BREAK! Lange Pause, sofort. LOS."))
+
+    time.sleep(60*int(long_brk))
+    
+    #stop long break
+    
+    print "Back to work..."
+    
+    t = Twitter(
+    	auth=OAuth('???','???','???','???'
+    t.direct_messages.new(
+    user="@???",
+    text="Noch eine Runde?"))
+    
+stop pomodori/start new pomodori
+    
+def wrapper():
+    pomodoro(one_pomodori,short_brk,long_brk,duration,duration_count)
+    print("A pomodori finished. Again?(y/n)")
+    
+    encore=raw_input(">")
+    if encore == "y":
+        wrapper()
+    elif encore == "n":
+        #tada
+        exit(0)
+    else:
+        print "I dont't get that one.Try again"
+
+if __name__ == "__main__":
+    wrapper()
+
+#-------------------------------------#
+#Many thanks to aungthurhahein - I modified some of this Ideas: https://github.com/aungthurhahein/pomodoro_python
+#-------------------------------------#
---
+++ readme_en_pomodoro_akut.md
@@ -0,0 +1,30 @@
+#EXPLANATION
+
+###TEST ENVIRONMENT:
+Mac OS X, Python 2.7.6
+
+##TASK:
+Tool for [Pomodoro-Technique](http://en.wikipedia.org/wiki/Pomodoro_Technique) - a website opens if the breaks are starting.
+
+##REALISATION:
+A python-script sends a reminder for short and long breaks as a twitter-dm via Twitter-API. At the same time a website opens in the default browser (for example: gym lessons by a physiotherapist). 
+
+##REQUIREMENTS:
+* a functional twitter-account
+
+##PROCEDURE:
+* registration to use the twitter-API
+* generation of the needed keys for OAuth and registration
+* definition of the values used for notification and alerting
+* modification of the python-script
+
+##FILES
+* pomodoro_akut.py 
+
+##Docs APIs and libraries:
+* [twitter-API](https://dev.twitter.com)
+* [python-library Twitter](https://pypi.python.org/pypi/twitter/1.15.0)
+
+# Pomodoro
+I took the idea and some code from here https://github.com/aungthurhahein/pomodoro_python.git. 
+Many thanks to aungthurhahein for sharing. 
